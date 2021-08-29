8fd75c6ef6176e96115f4f2a1f1d22889cf8158e
MubarakOke <okeolamubarak18@gmail.com>
2021-08-29 07:53:41 -0700

    edited form

--- registration/forms.py
+++ registration/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from registration.models import Professional
-from cloudinary.forms import CloudinaryFileField
+# from cloudinary.forms import CloudinaryFileField
 
 class ProfessionalForm(forms.ModelForm):
     # Personal information fields
@@ -28,7 +28,8 @@ class ProfessionalForm(forms.ModelForm):
     relevant_information=forms.CharField(required=True, label="Any other relevant information")
 
     # Image upload
-    work_evidence= CloudinaryFileField(required=True)
+    work_evidence= forms.FileField(required=True)
+
 
     class Meta:
         model= Professional
